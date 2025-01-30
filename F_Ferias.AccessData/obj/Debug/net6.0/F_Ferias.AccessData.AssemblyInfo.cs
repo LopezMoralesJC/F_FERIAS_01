e64b9bb3a324124005f41363d5131e1ed69957dd
juancarlos.lopez <juancarlos.lopez@stps.gob.mx>
2025-01-29 19:15:15 -0600

    martes

--- F_Ferias.AccessData/obj/Debug/net6.0/F_Ferias.AccessData.AssemblyInfo.cs
+++ F_Ferias.AccessData/obj/Debug/net6.0/F_Ferias.AccessData.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F_Ferias.AccessData")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f62e61969ee8436dcdc72cca41fb0d5d57667d01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79cafb3f190cbca185191c50e574ef798d4909cd")]
 [assembly: System.Reflection.AssemblyProductAttribute("F_Ferias.AccessData")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F_Ferias.AccessData")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
